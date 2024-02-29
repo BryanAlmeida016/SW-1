0789d2f89913bce2a8863489db6f660ffa97b61e
Bryan Almeida <124791559+BryanAlmeida016@users.noreply.github.com>
2024-02-29 12:26:58 -0300

    Add files via upload

--- Lampada/Lampada/Lampada.cs
+++ Lampada/Lampada/Lampada.cs
@@ -8,10 +8,52 @@ namespace Lampada
 {
     public class Lampada
     {
-        public bool Ligada { get; set; } 
-        public string? Cor { get; set; }
-        public int Potencia { get; set; }
+        private bool Ligada { get; set; } 
+        private string? Cor { get; set; }
+        private int Potencia { get; set; }
 
+        public void ligar()
+        { Ligada = true; }
 
+        public void desligar()
+        { Ligada = false; }
+
+        public bool estaLigada() 
+        { return Ligada; }
+
+        public void maisPotencia()
+        {
+            Potencia =+ 12;
+        }
+
+        public void menosPotencia()
+        {
+            Potencia = -12;
+        }
+
+        public int retornandoPotencia()
+        {
+            return Potencia;
+        }
+
+        public void Azul()
+        {
+            Cor = "Azul";
+        }
+
+        public void Vermelho()
+        {
+            Cor = "Vermelho";
+        }
+
+        public void Verde()
+        {
+            Cor = "Verde";
+        }
+
+        public string qualCor()
+        {
+            return Cor;
+        }
     }
 }
