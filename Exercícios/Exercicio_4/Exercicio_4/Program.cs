ce0fce21a03b2031e417a94c16acf69dcedbb9b8
Bryan Almeida <124791559+BryanAlmeida016@users.noreply.github.com>
2024-04-12 11:26:39 -0300

    Add files via upload

---
+++ Exercícios/Exercicio_4/Exercicio_4/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Exercicio_4;
+
+namespace Exercicío_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pagamento p = new Pagamento();
+            p.Nome = "Briano";
+            p.Sal = 7200;
+            p.HED = 0;
+            p.HEN = 8;
+            p.ND = 0;
+            p.Fal = 4;
+            p.DE = 3;
+            p.REF = 1;
+            p.Val = 0;
+            Console.WriteLine(p.FolhaDePagamento());
+        }
+    }
+}
